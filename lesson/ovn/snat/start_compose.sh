8689a220463a8b572b1357ce6cedd6f0d3e11da5
yuntao.cao <yuntao.cao@nocsys.com.cn>
2019-01-15 15:22:39 +0800

    set mtu to 1442

--- lesson/ovn/snat/start_compose.sh
+++ lesson/ovn/snat/start_compose.sh
@@ -2,6 +2,6 @@
 
 docker network rm ovnnet_local
 docker network rm ovnnet_outer
-docker network create --gateway 10.10.0.1 --subnet 10.10.0.0/24 ovnnet_local
-docker network create --gateway 10.20.0.1 --subnet 10.20.0.0/24 ovnnet_outer
+docker network create --gateway 10.10.0.1 --subnet 10.10.0.0/24 -o --mtu=1442 ovnnet_local
+docker network create --gateway 10.20.0.1 --subnet 10.20.0.0/24 -o --mtu=1442 ovnnet_outer
 docker-compose up --force-recreate
