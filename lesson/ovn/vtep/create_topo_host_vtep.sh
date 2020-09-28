504c469605b4c04db83850c97bd79b936df27ae9
sgkind <sunguanke11@mails.ucas.edu.cn>
2020-09-28 18:12:58 +0800

    fix bugs: * 将本地ip和ovn serverip修改为正确的值

--- lesson/ovn/vtep/create_topo_host_vtep.sh
+++ lesson/ovn/vtep/create_topo_host_vtep.sh
@@ -11,11 +11,11 @@ ovsdb-tool create /etc/openvswitch/vtep.db /usr/share/openvswitch/vtep.ovsschema
 ovs-appctl -t ovsdb-server ovsdb-server/add-db /etc/openvswitch/vtep.db
 ovs-appctl -t ovsdb-server ovsdb-server/add-remote ptcp:6640:0.0.0.0
 vtep-ctl add-ps br0
-vtep-ctl set Physical_Switch br0 tunnel_ips=192.168.220.17
+vtep-ctl set Physical_Switch br0 tunnel_ips=10.10.0.20
 /usr/share/openvswitch/scripts/ovs-vtep --log-file=/var/log/openvswitch/ovs-vtep.log --pidfile=/var/run/openvswitch/ovs-vtep.pid --detach br0
-ovs-vsctl set open . external-ids:ovn-remote=tcp:192.168.220.23:6642
+ovs-vsctl set open . external-ids:ovn-remote=tcp:10.10.0.10:6642
 ovs-vsctl set open . external-ids:ovn-encap-type=vxlan
-ovs-vsctl set open . external-ids:ovn-encap-ip=192.168.220.17
-ovn-controller-vtep -vconsole:emer -vfile:info --log-file=/var/log/openvswitch/ovn-controller-vtep.log --vtep-db=unix:/var/run/openvswitch/db.sock --ovnsb-db=tcp:192.168.220.23:6642 --detach --monitor 
+ovs-vsctl set open . external-ids:ovn-encap-ip=10.10.0.20
+ovn-controller-vtep -vconsole:emer -vfile:info --log-file=/var/log/openvswitch/ovn-controller-vtep.log --vtep-db=unix:/var/run/openvswitch/db.sock --ovnsb-db=tcp:10.10.0.10:6642 --detach --monitor 
 vtep-ctl add-ls ls0
 vtep-ctl bind-ls br0 vtep-gateway 0 ls0
